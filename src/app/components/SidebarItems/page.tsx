f57c2fbc5e8e8280ed9e7f101318f6102c1ba012
RathlavathRamesh <rameshrguktbasar@gmail.com>
2024-03-17 23:59:50 +0530

    Commit after Displaying the Sample users

--- src/app/components/SidebarItems/page.tsx
+++ src/app/components/SidebarItems/page.tsx
@@ -1,11 +1,12 @@
 import { VscKey } from "react-icons/vsc";
 import { GrInstagram } from "react-icons/gr";
 import { LuContact2 } from "react-icons/lu";
-import { FaGreaterThan } from "react-icons/fa6";
+import { FaGreaterThan } from "react-icons/fa";
 import React from "react";
 interface ItemProps {
-    IncoName: string;
+  svgElement:  JSX.Element;
     headingName: string;
+    id:number;
   }
   
   // Define the type for the props passed to the SideBarItem component
@@ -14,36 +15,17 @@ interface ItemProps {
   }
 
 const  SideBarItem: React.FC<SideBarItemProps>=(prop)=>{
-  //  const [collapse,setCollapse]=React.useState(0)
-    const {Item}=prop
-    console.log(Item)
-    let IconToRender;
-    switch (Item.IncoName) {
-      case "VscKey":
-        IconToRender = VscKey;
-        break;
-     case "GrInstagram":
-        IconToRender=GrInstagram;
-         break;
-     case "LuContact2":
-        IconToRender=LuContact2;
-        break;
-      default:
-        // Default to a placeholder icon or handle invalid icon names
-        IconToRender = LuContact2
-        break;
-    }
-  
+    const {Item}=prop 
     return (
-      <div  className="flex flex-row h-[7vh] justify-between items-center mt-3 rounded-lg text-gray-400   hover:bg-blue-700 hover:text-white hover:border-rounded">
+      <div  className="flex flex-row h-[7vh] cursor-pointer justify-between items-center mt-3 rounded-lg text-gray-400   hover:bg-blue-700 hover:text-white hover:border-rounded">
        <div className="flex flex-row">
        <div className="text-3xl pl-1">
-      <IconToRender className="text-3xl pl-1"  />
+        {Item.svgElement}
       </div>
-      <h1 className="text-2xl pl-3 hidden md:block">{Item.headingName}</h1>
+      <h1 className="text-2md pt-1 pl-3 hidden md:block">{Item.headingName}</h1>
        </div>
        <div>
-       <FaGreaterThan  className="cursor-pointer"/>
+       <FaGreaterThan  className="cursor-pointer hidden md:block"/>
        </div>
     </div>
     ) 
